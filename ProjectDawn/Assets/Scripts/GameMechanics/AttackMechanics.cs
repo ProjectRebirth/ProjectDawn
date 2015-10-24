7231fd7402b44d31ccf692b9c4dde48ca2957bcf
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-10-24 17:04:10 -0400

    Updated script files

---
+++ ProjectDawn/Assets/Scripts/GameMechanics/AttackMechanics.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackMechanics : MonoBehaviour {
+    public float[] attackDuration;//The amount of time that each attack needs complete
+    public float[] attackDelay;//A delay before the hitbox for this attack is set to active
+    public Collider2D[] hitBoxes;//The hitboxes needed for each attack
+    public float[] attackDamage;
+
+
+    private int currentAttack;
+
+    void Update()
+    {
+        updateTimers();
+    }
+
+    void updateTimers()
+    {
+        for (int i = 0; i < attackDelay.Length; i++)
+        {
+            attackDelay[i] = Mathf.MoveTowards(attackDelay[i], 0, Time.deltaTime);
+        }
+        for (int i = 0; i < attackDuration.Length; i++)
+        {
+            attackDuration[i] = Mathf.MoveTowards(attackDuration[i], 0, Time.deltaTime);
+        }
+    }
+
+
+    public void attack(bool attackButton)
+    {
+
+    }
+
+    
+}
---
+++ ProjectDawn/Assets/Scripts/GameMechanics/Health.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+    public int maxHealth = 100;
+    public int currentHealth;
+    public float timeToRemove = 5;//The amount of the time the player will be active before being deleted from the game
+
+    private float removeTimer;
+    private bool isDead;
+
+
+
+
+    void Start()
+    {
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    void Update()
+    {
+        if (isDead)
+        {
+            removeTimer -= Time.deltaTime;
+            if (removeTimer < 0f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    public void takeDamage(int damageApplied)
+    {
+        currentHealth -= damageApplied;
+        checkIsDead();
+    }
+
+    void checkIsDead()
+    {
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            isDeadCleanUp();
+        }
+    }
+
+
+    protected virtual void isDeadCleanUp() 
+    {
+        isDead = true;
+    }
+
+
+    public bool getIsDead()
+    {
+        return isDead;
+    }
+}
