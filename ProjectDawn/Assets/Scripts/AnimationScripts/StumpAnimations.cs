e45eab15709aad5d9b348f0bd7e830c4b5cdf968
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-07 15:00:47 -0500

    Back and forth AI added

---
+++ ProjectDawn/Assets/Scripts/AnimationScripts/StumpAnimations.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StumpAnimations : MonoBehaviour {
+    Animator anim;
+    WalkMechanics walkMechanics;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        walkMechanics = GetComponent<WalkMechanics>();
+    }
+
+    void Update()
+    {
+        anim.SetFloat("speed", Mathf.Abs(walkMechanics.getHInput()));
+    }
+}
---
+++ ProjectDawn/Assets/Scripts/ArtificialIntelligence/SideMoveAI.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(WalkMechanics))]
+
+public class SideMoveAI : MonoBehaviour {
+    public Transform[] positions;
+
+    int currentDestination;
+    Vector3 lastPosition;
+    Vector3[] positionVectors;
+    WalkMechanics walkMechanics;
+    float moveDirection;
+    
+    void Start()
+    {
+        
+        walkMechanics = GetComponent<WalkMechanics>();
+        setDestinations();
+        moveDirection = calculateMoveDirection();
+    }
+
+    void Update()
+    {
+        print(currentDestination);
+        updateMovement();
+        updatePosition();
+    }
+
+    void updateMovement()
+    {
+
+        if (passedDestination())
+        {
+            currentDestination = (currentDestination + 1) % positionVectors.Length;
+            moveDirection = calculateMoveDirection();
+        }
+        walkMechanics.moveHorizontal(moveDirection);
+    }
+
+    bool passedDestination()
+    {
+        float check1 = lastPosition.x - positionVectors[currentDestination].x;
+        float check2 = transform.position.x - positionVectors[currentDestination].x;
+
+        if (check1 < 0 && check2 < 0)
+        {
+            return false;
+        }
+        if (check1 > 0 && check2 > 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    float calculateMoveDirection()
+    {
+        float distance = positionVectors[currentDestination].x - transform.position.x;
+        if (distance < 0)
+        {
+            return -1;
+        }
+        return 1;
+    }
+
+    void updatePosition()
+    {
+        lastPosition = transform.position;
+    }
+
+    void setDestinations()
+    {
+        positionVectors = new Vector3[positions.Length];
+        int i = 0;
+        foreach(Transform t in positions)
+        {
+            positionVectors[i] = positions[i].position;
+            i++;
+        }
+    }
+}
